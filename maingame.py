0a9f2e0ee38adf39526fb54de6227207cecfa46f
Manuel Mayssen <manuel.mayssen@gmail.com>
2017-02-25 22:22:02 +0100

    Created maingame.py
    Introducing general answer structure.

---
+++ maingame.py
@@ -0,0 +1,105 @@
+#General answer structure
+#	def functionNr##():
+#    print("Answer options:")
+#    print("1) First answer")
+#    print("2) Second answer")
+#    print("3) Third answer")
+#    answerNr## = int(input("Choose an answer: "))
+#    if answerNr == 1:
+#        answerNrsubAnswer1()
+#    elif answerNR == 2:
+#        answerNrsubAnswer2()
+#    elif answerNR == 3:
+#        answerNrsubAnswer3()
+#    else:
+#        print("Enter a valid answer!")
+#        functionNr##()
+
+
+
+#start
+def prestart():
+    print("Hey sucker! Whats your name?")
+    name = input("Enter your name: ")
+    print(name, ", you're a dirty boy. You even managed to enter your name!")
+
+def start():
+    print("Answer options:")
+    print("1) WTF?!")
+    print("2) Yeah. I got homeschooled and now I'm a nerd without any friends...")
+    print("3) You dirty.")
+    answer1 = int(input("Choose an answer: "))
+    if answer1 == 1:
+        ans11()
+    elif ans1 == 2:
+        ans12()
+    elif ans1 == 3:
+        ans13()
+    else:
+        print("Enter a valid answer, sucker!")
+        start()
+
+#questions
+def ans11():
+    print("You think u tough?! I'll fuck you up, bitch!")
+    print("Answer options:")
+    print("1) WTF?!")
+    print("2) Yeah. I got homeschooled and now I'm a nerd without any friends...")
+    print("3) You dirty.")
+    answer11 = int(input("Choose an answer: "))
+    if answer11 == 1:
+        ans111()
+    elif answerwtf == 2:
+        ans112()
+    elif answerwtf == 3:
+        ans113()
+    else:
+        print("Enter a valid answer, sucker!")
+        ans11()
+
+def ans12():
+    print("So you never fucked a chick?!")
+    answer12 = input("Choose an answer: ")
+    if answer12 == 1:
+        dosmth()
+    elif answer12 == 2:
+        dosmth()
+    elif answer12 == 3:
+        dosmth()
+    else:
+        print("Enter a valid answer, sucker!")
+        nerd()
+def ans13():
+    print("You call me sucker? You're a slav!")
+    print("Answer options:")
+    print("1) WTF?!")
+    print("2) Yeah. I got homeschooled and now I'm a nerd without any friends...")
+    print("3) You dirty.")
+    answer13 = int(input("Choose an answer: "))
+    if answerwtf == 1:
+        dosmth()
+    elif answerwtf == 2:
+        dosmth()
+    elif answerwtf == 3:
+        dosmth()
+    else:
+        print("Enter a valid answer, sucker!")
+        ans13()
+
+#Bitte an standards anpassen!!!
+def what():
+	print("Your mama said what once, after i showed her my Dongogo!")
+    print("Answer options:")
+    print("1) WTF?!")
+    print("2) Yeah. I got homeschooled and now I'm a nerd without any friends...")
+    print("3) You dirty.")
+    answerwtf = int(input("Choose an answer: "))
+    if answerwtf == 1:
+        dosmth()
+    elif answerwtf == 2:
+        dosmth()
+    elif answerwtf == 3:
+        dosmth()
+    else:
+        print("Enter a valid answer, sucker!")
+        what()
